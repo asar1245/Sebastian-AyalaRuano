23babf89be41da5c1c3d740155f9db2b41c21730
asar1245 <sebasar1245@gmail.com>
2021-09-08 20:10:41 -0500

    New deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,26 +1,45 @@
-# certain configuration, run the `setup.sh` script to configure this. See
-# https://hjdskes.github.io/blog/deploying-hugo-on-personal-github-pages/index.html
-# for more information.
-# Set the English locale for the `date` command.
-export LC_TIME=en_US.UTF-8
+#!/bin/bash
+# Script for publish personal website with GitPages
 # GitHub username.
 USERNAME=sayalaruano
 # Name of the branch containing the Hugo source files.
 SOURCE=source
-# The commit message.
-MESSAGE="Site rebuild $(date)"
-msg() {
-    printf "\033[1;32m :: %s\n\033[0m" "$1"
-}
-msg "Pulling down the \`master\` branch into \`public\` to help avoid merge conflicts"
-git subtree pull --prefix=public \
-    git@github.com:$USERNAME/$USERNAME.github.io.git origin master -m "Merge origin master"
-msg "Building the website"
-hugo
-msg "Pushing the updated \`public\` folder to the \`$SOURCE\` branch"
-git add public
-git commit -m "$MESSAGE"
-git push origin "$SOURCE"
-msg "Pushing the updated \`public\` folder to the \`master\` branch"
-git subtree push --prefix=public \
-    git@github.com:$USERNAME/$USERNAME.github.io.git master
+
+msg "Checking that the current branch is the source"
+git checkout $SOURCE
+
+msg "Deleting current version of the public directory "
+rm -rf public/
+
+msg "Building the website with hugo - you have to install this program in your computer"
+# The instalaltion tutorial is here: https://gohugo.io/getting-started/installing/
+hugo -d ./public
+
+msg "Adding all the new files in the source branch and make a commit"
+git add -A && git commit -m "Upgrade personal webpage"
+
+msg "Pushing the changes to the GitHub repo" 
+git push origin $SOURCE 
+
+msg "Deleting the master branch from the local an external repo"
+git branch -D master
+git push origin --delete master
+
+msg "Creating an empty orphan new master branch"
+git checkout --orphan master
+
+msg "Deleting all the contents in the master branch"
+git rm -rf .
+
+msg "Grabbing the public folder from the source to master branch"
+git checkout source public/*
+
+msg "Extracting the files outside the folder adn deleting the folder"
+cp -r public/* ./
+rm -r public
+
+msg "Adding all the new files in the master branch and make a commit"
+git add -A && git commit -m "Upgrade personal webpage"
+
+msg "Pushing the changes to the GitHub repo" 
+git push origin master 
